341791a00caede7d932d5454dc8ceeeabc2bd0a5
Eduardo P de Sousa <edupsousa@gmail.com>
2024-05-02 19:53:00 -0300

    create nix flake for node 18 development

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Node 18 development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          nodejs = prev.nodejs_18;
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs ];
+        };
+      });
+    };
+}
